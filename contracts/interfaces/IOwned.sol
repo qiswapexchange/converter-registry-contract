f0eb5c01cdf9185e68e758037d1d18ea61e4c616
snowwhitedev <snowwhite91924@gmail.com>
2021-08-17 23:58:28 +0700

    feat: first commit

---
+++ contracts/interfaces/IOwned.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.0;
+
+/*
+    Owned contract interface
+*/
+interface IOwned {
+    // this function isn't abstract since the compiler emits automatically generated getter functions as external
+    function owner() external view returns (address);
+
+    function transferOwnership(address _newOwner) external;
+    function acceptOwnership() external;
+}
